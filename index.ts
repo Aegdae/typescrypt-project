7ca0e77c46ae26c7d6ed22682d6f04965f7bba29
Aegdae <jonnathasg@gmail.com>
2025-04-22 21:19:42 -0300

    Porjeto TS

--- index.ts
+++ index.ts
@@ -5,19 +5,3 @@ import Nivel from "./models/enums";
 
 
 const sistema = new SistemaCadastro();
-
-const cursoJava = sistema.addCurso("curso backend", "Java", 300, Nivel.Iniciante);
-const cursoC = sistema.addCurso("curso backend", "C", 400, Nivel.Intermediario);
-const cursoJavaScript = sistema.addCurso("curso frontend", "JavaScript", 150, Nivel.Avancado);
-
-
-const alunoJonnathas = sistema.addAluno("Jonnathas", 27, cursoJava);
-const alunoJoana = sistema.addAluno("Joana", 49);
-
-
-cursoJava.descricao = "Curso de Java com Jonnathas"
-
-console.log(cursoJava)
-
-
-console.log(alunoJonnathas.exibirDados)
